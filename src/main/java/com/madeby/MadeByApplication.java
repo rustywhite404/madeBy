80e6d02249d10fa84c84bd98e49088436997ef12
rustywhite404 <rustywhite404@gmail.com>
2024-12-18 11:14:33 +0900

    [init] 초기 환경설정

---
+++ src/main/java/com/madeby/MadeByApplication.java
@@ -0,0 +1,13 @@
+package com.madeby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadeByApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MadeByApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/madeby/MadeByApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madeby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadeByApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
