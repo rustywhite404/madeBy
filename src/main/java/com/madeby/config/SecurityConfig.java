456f3e285333aadfc58a9af7b14ecc90a06b349e
rustywhite404 <rustywhite404@gmail.com>
2024-12-21 15:55:18 +0900

    [feat] 로그인/권한이 필요한 기능에 비로그인, 비권한 접근 시 인증/인가 필요 error response 반환

--- src/main/java/com/madeby/config/SecurityConfig.java
+++ src/main/java/com/madeby/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.madeby.security.JwtAuthenticationFilter;
 import com.madeby.security.JwtAuthorizationFilter;
 import com.madeby.security.UserDetailsServiceImpl;
 import com.madeby.util.JwtUtil;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
@@ -15,7 +16,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -65,23 +68,44 @@ public class SecurityConfig {
                         .requestMatchers("/").permitAll() // 메인 페이지 요청 허가
                         .requestMatchers("/api/user/**").permitAll() // '/api/user/'로 시작하는 요청 모두 접근 허가
                         .requestMatchers("/api/products/**").permitAll() // '/api/products/'로 시작하는 요청 모두 접근 허가
+                        .requestMatchers("/api/orders/**").authenticated() // 인증 필요
                         .anyRequest().authenticated() // 그 외 모든 요청 인증처리
         );
-
-//        http.formLogin((formLogin) ->
-//                formLogin
-//                        .loginPage("/api/user/login-page").permitAll()
-//        );
 
         // Spring Security 기본 로그인 폼 비활성화
         http.formLogin((formLogin) -> formLogin.disable());
         http.httpBasic((httpBasic) -> httpBasic.disable());
+
+        // 인증 및 권한 실패 시 반환 핸들러 설정
+        http.exceptionHandling((exceptionHandling) -> exceptionHandling
+                .authenticationEntryPoint(authenticationEntryPoint()) // 인증 실패 핸들러
+                .accessDeniedHandler(accessDeniedHandler()) // 권한 부족 핸들러
+        );
 
         // 필터 관리
         http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
         http.addFilterBefore(jwtAuthorizationFilter(), JwtAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    // 인증 실패 (로그인이 필요할 때)
+    @Bean
+    public AuthenticationEntryPoint authenticationEntryPoint() {
+        return (request, response, authException) -> {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401
+            response.setContentType("application/json; charset=UTF-8"); // UTF-8 설정
+            response.getWriter().write("{\"error\": \"로그인이 필요합니다.\"}");
+        };
+    }
+
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+        return (request, response, accessDeniedException) -> {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN); // 403
+            response.setContentType("application/json; charset=UTF-8"); // UTF-8 설정
+            response.getWriter().write("{\"error\": \"권한이 부족합니다.\"}");
+        };
     }
 }
 
