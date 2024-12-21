bcacc66df270718b4266a6d3cf17b65a79afc64a
rustywhite404 <rustywhite404@gmail.com>
2024-12-21 16:56:08 +0900

    [fix] loadUserByUsername : 로그인 시 입력된 평문 이메일과 JWT에서 추출된 해시된 이메일을 구분해서 처리하도록 수정
    - 로그인 시 입력된 평문 이메일과 JWT에서 추출된 해시된 이메일이 모두 loadUserByUsername 메서드로 전달되는데, 이 메서드에서 두 경우를 따로 처리하지 못해서 로그인 후 Access Token이 적절한 사용자와 매칭되지 못했던 문제를 수정함

--- src/main/java/com/madeby/security/UserDetailsServiceImpl.java
+++ src/main/java/com/madeby/security/UserDetailsServiceImpl.java
@@ -23,12 +23,16 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 
         String emailHash = null;
-        try {
-            emailHash = AES256Util.hashEmail(email);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        // 입력된 email이 이미 해시된 값인지 확인
+        if (email.contains("@")) { // 평문 이메일일 경우
+            try {
+                emailHash = AES256Util.hashEmail(email);
+            } catch (Exception e) {
+                throw new RuntimeException("이메일 해시 처리 중 오류 발생", e);
+            }
+        } else { // 이미 해시된 값일 경우
+            emailHash = email;
         }
-
         // 사용자 조회
         User user = userRepository.findByEmailHash(emailHash)
                 .orElseThrow(() -> new MadeByException(MadeByErrorCode.USER_NOT_FOUND));
