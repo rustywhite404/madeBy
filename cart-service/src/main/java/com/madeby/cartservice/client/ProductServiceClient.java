a78e50025ddc0eb36d6fac4de8bbab63a85efed7
rustywhite404 <rustywhite404@gmail.com>
2025-01-07 12:26:10 +0900

    [fix] Test용 Circuit Breaker, Retry 코드 제거

--- cart-service/src/main/java/com/madeby/cartservice/client/ProductServiceClient.java
+++ cart-service/src/main/java/com/madeby/cartservice/client/ProductServiceClient.java
@@ -1,15 +1,12 @@
 package com.madeby.cartservice.client;
 
-import com.madeBy.shared.common.ApiResponse;
 import com.madeby.cartservice.dto.ProductInfoDto;
 import com.madeby.cartservice.dto.ProductsDto;
-import com.madeby.cartservice.fallback.ProductServiceFallback;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name = "product-service", fallback = ProductServiceFallback.class)
+@FeignClient(name = "product-service")
 public interface ProductServiceClient {
 
     @GetMapping("/api/products/{productId}")
